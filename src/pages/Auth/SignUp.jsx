e378db6a6841c6d457fa3705d286f2d95ed60fab
Ayida-Aman <amanayida1@gmail.com>
2025-06-01 09:11:49 +0300

    feat(payments): Set up backend API using Firebase functions

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useContext, useState } from 'react'
 import classes from "./SignUp.module.css"
 import Layout from '../../components/Layout/Layout'
